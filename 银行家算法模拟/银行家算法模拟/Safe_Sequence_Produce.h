2beeb496e1ad68eaa11a2ac45bd88ec8e351a24e
chenraozhong <34003402+chenraozhong@users.noreply.github.com>
2019-03-10 17:35:45 +0800

    此次提交是为了测试，查看bug，无其他意义

--- 银行家算法模拟/银行家算法模拟/Safe_Sequence_Produce.h
+++ 银行家算法模拟/银行家算法模拟/Safe_Sequence_Produce.h
@@ -15,14 +15,16 @@ private:
 	vector<int> m_Source;
 	vector<int> m_Remain_Source;
 	vector<int, vector<int> > m_Allocation;
-	vector<int, vector<int> > m_MAXRequest;//客户对资源的需求量
-	vector<int, vector<int> > m_Request;
-	vector<int, int> m_Duration;
+//	vector<int, vector<int> > m_MAXRequest;//客户对资源的需求量
+//	vector<int, vector<int> > m_Request;
+//	vector<int, int> m_Duration;
 	/*
 	vector<map<int, string> > m_Release: string由用户编码+时间点构成。
 	例如：m_Release[2]=<5,1_9>; 表明2号资源在第9时刻被1号客户释放，释放量为5
 	*/
+	/*
 	vector<map<int, string> >  m_Release;//资源释放的时间点和量
+	*/
 public:
 	void Sqe_Messageinit();
 	void Sqe_Sourseinit();
--- 银行家算法模拟/银行家算法模拟/银行家算法模拟.cpp
+++ 银行家算法模拟/银行家算法模拟/银行家算法模拟.cpp
@@ -2,12 +2,20 @@
 //
 #include "stdafx.h"
 #include "Init_Message.h"
+#include"Safe_Sequence_Produce.h"
+#include<iostream>
+#include<sstream>
+#include<string>
+using namespace std;
+
 int main()
 {
+	/*
 	InitCls my_AvaliableNumber(4, 4);
 	my_AvaliableNumber.InitRandomOfAvailable();
 	my_AvaliableNumber.InitRandomOfMax();
 	my_AvaliableNumber.InitRandomOfAllocationAndTime();
+	*/
 	system("pause");
 	return 0;
 }
